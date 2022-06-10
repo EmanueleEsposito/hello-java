12b7c497047d8f5acb6f74cb56b7cd7db228bfeb
EmanueleEsposito <manu2022@blu.it>
2022-06-10 17:39:41 +0200

    primo commento

---
+++ src/org/generation/italy/StampaSaluto.java
@@ -0,0 +1,11 @@
+package org.generation.italy;
+
+public class StampaSaluto {
+
+	public static void main(String[] args) {
+		System.out.println("HELLO JAVA");
+		// TODO Auto-generated method stub
+
+	}
+
+}
